77b4abf03418da81f509f92e5e527b9cb21bee27
Anshika Bhadoria <anshikabhadoria88@gmail.com>
2025-06-09 11:23:04 +0530

    chng cta

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,12 +4,13 @@ import { Hero } from "../components/Hero";
 import { ServicesSection } from "../components/ServicesSection";
 // import { TestimonialsSection } from "../components/TestimonialsSection";
 import { SellingOptionsSection } from "../components/SellingOption";
-import { CtaSection } from "../components/CtaSection";
+// import { CtaSection } from "../components/CtaSection";
 import Banner from "../components/Banner";
 import { ExploreCitiesSection } from "../components/ExploreCitiesSection";
 import AgentsSection from "../components/agentsSection";
 import FeaturedProperty from "../components/FeaturedProperty";
 import Foote from "../components/Foote";
+import CtaSection from "../components/CtaSection";
 
 const Home = () => {
   return (
@@ -26,6 +27,7 @@ const Home = () => {
         <SellingOptionsSection />
 
         <AgentsSection />
+        {/* <CtaSection /> */}
         <CtaSection />
       </main>
       <Foote />
